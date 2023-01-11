852fdc87813246fb75184bbec272a6875d1f242f
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-11 23:40:54 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="../dist/output.css">
+		<title>Welcome to our local library</title>
+	</head>
+
+	<body>
+		<?php require_once __DIR__ . '/includes/header.php'; ?>
+		<main>
+			<h1 class="text-red-400 text-4xl">Welcome to our local library</h1>
+		</main>
+		<?php require_once __DIR__ . '/includes/footer.php'; ?>
+	</body>
+
+</html>
