8da42a68191282da490062cc3e9db6884c08daed
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-11 23:39:58 +0200

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<header>
+	<div>
+		LOGO
+	</div>
+	<nav>
+		<ul>
+			<li><a href="" class="">nav-item-1</a></li>
+			<li><a href="" class="">nav-item-2</a></li>
+			<li><a href="" class="">nav-item-3</a></li>
+		</ul>
+		<ul>
+			<li><a href="/library-app/templates/login/index.html">Login</a></li>
+			<li><a
+					href="/library-app/templates/register/index.html">Register</a>
+			</li>
+		</ul>
+	</nav>
+</header>
