e48b242ab9a1454ef02131fc4738d27ce29a25f6
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-11 23:40:03 +0200

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<?php
+$site_name = 'library';
+?>
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<p class="text-center">© 2017 - <?php echo date('Y'); ?> - <a
+						href="http://www.<?php echo $site_name; ?>.com"
+						target="_blank"><?php echo $site_name; ?></a> - All
+					Rights Reserved</p>
+			</div>
+		</div>
+	</div>
+</footer>
