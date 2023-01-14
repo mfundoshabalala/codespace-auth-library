2a58890121ee7854d372a6e99f68bebbf693a9c2
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-14 03:03:05 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,26 @@
 <header>
-	<div>
-		LOGO
-	</div>
 	<nav>
+		<div>
+			<a href="/library-app/" class="logo">Local Library</a>
+		</div>
 		<ul>
-			<li><a href="" class="">nav-item-1</a></li>
-			<li><a href="" class="">nav-item-2</a></li>
-			<li><a href="" class="">nav-item-3</a></li>
+			<li><a href="/library-app/">Home</a></li>
+			<li><a href="/library-app/templates/catalogue.php">Books</a></li>
+			<li><a href="/library-app/templates/add-book.php">Add books</a></li>
 		</ul>
-		<ul>
-			<li><a href="/library-app/templates/login/index.html">Login</a></li>
-			<li><a
-					href="/library-app/templates/register/index.html">Register</a>
-			</li>
-		</ul>
+		<?php
+		if (isset($_SESSION['logged_in'])) {
+			echo '<form action="/library-app/includes/logout-include.php" method="post">
+						<input type="submit" name="submit" value="Logout" class="btn-danger">
+					</form>';
+		} else {
+			echo '
+			<ul>
+				<li><a href="/library-app/templates/login.php">Login</a></li>
+				<li><a href="/library-app/templates/register.php">Register</a></li>
+			</ul>
+			';
+		}
+		?>
 	</nav>
 </header>
