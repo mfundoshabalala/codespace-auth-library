e5fa1020aa083fbe7ae086feca34a09caca3c80d
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-16 00:42:18 +0200

    Create search.component.php

---
+++ templates/components/search.component.php
@@ -0,0 +1,9 @@
+<form method="post" class="mx-auto min-w-max p-4">
+	<input type="text" name="search"
+		placeholder="Search for book, author or genre" />
+	<div class="flex justify-around gap-2">
+		<input type="submit" name="submit" value="Search"
+			class="btn-secondary" />
+		<input type="reset" name="reset" value="Reset" class="btn-danger" />
+	</div>
+</form>
