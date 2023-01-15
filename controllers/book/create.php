23c7a8a307309a83fd3f053e18d5f6769413da36
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-16 00:38:52 +0200

    Create create.php

---
+++ controllers/book/create.php
@@ -0,0 +1,25 @@
+<?php
+// set the heading and require the template
+$heading = 'Add a Book to the Collection';
+
+require 'config/index.php';
+$db = new Database($config['database']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// dd($_POST);
+	$db->insert("INSERT INTO books (book_name, year, genre, age_group) VALUES (?, ?, ?, ?)", [
+		$_POST['bookName'],
+		$_POST['bookYear'],
+		$_POST['bookGenre'],
+		$_POST['bookAgeGroup'],
+	]);
+
+	$bookId = $db->lastInsertId();
+
+	$db->insert("INSERT INTO authors (author_name, genre, age, book_id) VALUES (?, ?, ?, ?)", [
+		$_POST['authorName'],
+		$_POST['authorGenre'],
+		$_POST['authorAge'],
+		$bookId,
+	]);
+}
+require 'templates/book/create.template.php';
