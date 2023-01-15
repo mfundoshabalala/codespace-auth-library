1b2921211c4385a5983d437460e7b0160f9f5ed1
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-16 00:41:05 +0200

    Create index.php

---
+++ router/index.php
@@ -0,0 +1,28 @@
+<?php
+
+// require 'config\index.php';
+// Get the URI
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+// Abort function
+function abort($code = 404)
+{
+	http_response_code($code);
+	require 'templates/' . $code . '.template.php';
+	die();
+}
+
+$configs = require 'config\index.php';
+
+// Route to the controller
+function routeToController($uri, $routes)
+{
+	// Check if the URI exists in the routes array
+	if (array_key_exists($uri, $routes)) {
+		require $routes[$uri];
+	} else {
+		abort();
+	}
+};
+
+routeToController($uri, $config['routes']);
