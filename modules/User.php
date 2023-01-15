5884c3bb09af88399508eafd11a409c6e6a2fed1
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-16 00:40:48 +0200

    Create User.php

---
+++ modules/User.php
@@ -0,0 +1,64 @@
+<?php
+
+class User
+{
+	public function __construct()
+	{
+		#code here
+	}
+
+	public function __destruct()
+	{
+		#code here
+	}
+
+	public function login($username, $email, $password)
+	{
+		#code here
+	}
+
+	public function logout()
+	{
+		#code here
+	}
+
+	public function register()
+	{
+		#code here
+	}
+
+	public function validate()
+	{
+		#code here
+	}
+
+	public function get()
+	{
+		#code here
+	}
+
+	public function set()
+	{
+		#code here
+	}
+
+	public function delete()
+	{
+		#code here
+	}
+
+	public function update()
+	{
+		#code here
+	}
+
+	public function getRole()
+	{
+		#code here
+	}
+
+	public function setRole()
+	{
+		#code here
+	}
+}
