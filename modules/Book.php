b7a4728973df5099055e0abf117c14b10d99172b
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-16 00:40:33 +0200

    Create Book.php

---
+++ modules/Book.php
@@ -0,0 +1,34 @@
+<?php
+
+class Book
+{
+	public function __construct()
+	{
+		# code...
+	}
+
+	public function __destruct()
+	{
+		# code...
+	}
+
+	public function get()
+	{
+		# code...
+	}
+
+	public function set()
+	{
+		# code...
+	}
+
+	public function delete()
+	{
+		# code...
+	}
+
+	public function update()
+	{
+		# code...
+	}
+}
