9ef1f58d775b5d3f0234e60c2bc2ddf599cccffb
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-16 00:40:04 +0200

    Delete logout-include.php

--- includes/logout-include.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Path: includes\logout-include.php
-//  logout the user and redirect to login page
-session_start();
-session_unset();
-session_destroy();
-header("Location: ../templates/login.php");
-exit;
