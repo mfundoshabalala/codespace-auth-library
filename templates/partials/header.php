f4ab3b45c1bf2879468e8fb32dcbaee1232d2c2f
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:31:14 +0200

    Update header.php

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,44 +1,30 @@
-<div class="relative bg-white">
-	<div class="mx-auto px-6">
-		<div
-			class="flex items-center justify-between border-b-2 border-gray-100 py-4 md:justify-start md:space-x-10">
+<div class="relative">
+	<div class="px-6 mx-auto">
+		<div class="flex items-center justify-between py-4 border-b-2 border-gray-800 md:justify-start md:space-x-10">
 			<div class="flex justify-start lg:w-0 lg:flex-1">
 				<a href="/">
 					<span class="sr-only">Your Company</span>
-					<img class="h-8 w-auto sm:h-10"
-						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-						alt="">
+					<img class="w-auto h-8 sm:h-10" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
 				</a>
 			</div>
 			<div class="-my-2 -mr-2 md:hidden">
-				<button type="button"
-					class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-					aria-expanded="false">
+				<button type="button" class="inline-flex items-center justify-center p-2 bg-white rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
 					<span class="sr-only">Open menu</span>
-					<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg"
-						fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						stroke="currentColor" aria-hidden="true">
-						<path stroke-linecap="round" stroke-linejoin="round"
-							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+					<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 					</svg>
 				</button>
 			</div>
 			<nav class="hidden space-x-10 md:flex">
-				<a href="/"
-					class="text-base font-medium hover:text-gray-900">Home</a>
-				<a href='/books'
-					class="text-base font-medium hover:text-gray-900">Book
+				<a href="/" class="text-base font-medium">Home</a>
+				<a href='/books' class="text-base font-medium">Book
 					Catalogue</a>
-				<a href="/book/create"
-					class="text-base font-medium hover:text-gray-900">Add
+				<a href="/book/create" class="text-base font-medium">Add
 					Book</a>
 			</nav>
-			<div
-				class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-				<a href="/login"
-					class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Login</a>
-				<a href="/register"
-					class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Register</a>
+			<div class="items-center justify-end hidden md:flex md:flex-1 lg:w-0">
+				<a href="/login" class="text-base font-medium whitespace-nowrap ">Login</a>
+				<a href="/register" class="inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700">Register</a>
 			</div>
 		</div>
 	</div>
