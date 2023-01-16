659661dd41fe869b35e0d0d3272b96ad8fd5fdb5
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:30:02 +0200

    Update index.php

--- controllers/book/index.php
+++ controllers/book/index.php
@@ -1,11 +1,14 @@
 <?php
 require 'config/index.php';
-// set the heading, query the database for book and require the template
-$heading = 'Unlock the Pages of Knowledge';
 // get the book by its id from the database and require the template
 $db = new Database($config['database']);
 $book = $db->query("SELECT * FROM books_authors WHERE book_id = ?", [$_GET['id']]);
 // dd($book);
 // get a list of books by the same author
 // $books = $db->query();
+
+
+// set the heading, query the database for book and require the template
+$heading = 'Unlock the Pages of Knowledge';
+$gradient = 'from-slate-700 via-teal-600 to-slate-700';
 require 'templates/book/index.template.php';
