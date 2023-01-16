c329afc1b77ef33a18a20b89b806ce4569d13a8b
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:30:06 +0200

    Update books.php

--- controllers/books.php
+++ controllers/books.php
@@ -6,4 +6,5 @@ $db = new Database($config['database']);
 $books = $db->query('SELECT * FROM books_authors');
 // set the heading and require the template
 $heading = 'Library Collection';
+$gradient = 'from-slate-700 via-teal-600 to-slate-700';
 require 'templates/books.template.php';
