12e41bba1a8ad63d20bbe9bb44c13bd71bb79802
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:30:53 +0200

    Update index.template.php

--- templates/book/index.template.php
+++ templates/book/index.template.php
@@ -3,30 +3,25 @@
 <?php require 'templates/partials/banner.php'; ?>
 
 <!-- ui for displaying a single book using tailwindcss -->
-<div class="container mx-auto px-4 max-w-5xl">
+<div class="container max-w-5xl px-4 mx-auto">
 	<div class="flex flex-wrap -mx-1 lg:-mx-4">
-		<div class="my-1 px-1 w-full lg:my-4 lg:px-4 lg:w-1/2">
+		<div class="w-full px-1 my-1 lg:my-4 lg:px-4 lg:w-1/2">
 			<article class="overflow-hidden rounded-lg shadow-lg">
 				<a href="#">
-					<img alt="Placeholder" class="block h-auto w-full"
-						src="https://picsum.photos/600/400/?random">
+					<img alt="Placeholder" class="block w-full h-auto" src="https://picsum.photos/600/400/?random">
 				</a>
-				<header
-					class="flex items-center justify-between leading-tight p-2 md:p-4">
+				<header class="flex items-center justify-between p-2 leading-tight md:p-4">
 					<h1 class="text-lg">
-						<a class="no-underline hover:underline text-black"
-							href="#">
+						<a class="no-underline hover:underline" href="#">
 							<?= $book['book_name']; ?>
 						</a>
 					</h1>
-					<p class="text-grey-darker text-sm">
+					<p class="text-sm">
 						<?= $book['author_name']; ?>
 					</p>
 				</header>
-				<footer
-					class="flex items-center justify-between leading-none p-2 md:p-4">
-					<a class="flex items
-					center no-underline hover:underline text-black" href="#">
+				<footer class="flex items-center justify-between p-2 leading-none md:p-4">
+					<a class="flex no-underline items center hover:underline" href="#">
 						<p class="ml-2 text-sm">
 							<?= $book['genre']; ?>
 						</p>
