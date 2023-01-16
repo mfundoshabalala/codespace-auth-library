50f11964fd0714f2ac1aa9e3d9d35ecb4c14cfd7
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:31:05 +0200

    Update index.template.php

--- templates/index.template.php
+++ templates/index.template.php
@@ -2,5 +2,8 @@
 <?php require 'partials/header.php'; ?>
 <?php require 'partials/banner.php'; ?>
 
+<main class="flex-1 flex flex-col justify-center items-center">
+	<h2 class="text-3xl font-bold">Welcome to the world of knowledge.</h2>
+</main>
 
 <?php require 'partials/footer.php'; ?>
