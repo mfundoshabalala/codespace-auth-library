cbae8d1d53f06173af088e687c16be5379c92ee6
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:30:14 +0200

    Update login.php

--- controllers/login.php
+++ controllers/login.php
@@ -1,5 +1,8 @@
 <?php
 
+require 'modules/User.php';
+$user = new User();
 // set the heading and require the template
 $heading = 'Access your account';
+$gradient = 'from-slate-700 via-sky-600 to-slate-700';
 require 'templates/login.template.php';
