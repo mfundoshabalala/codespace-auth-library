41a6106e968218fd8d0b5679e28a3adb6d9de166
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:31:00 +0200

    Update forgot-password.template.php

--- templates/forgot-password.template.php
+++ templates/forgot-password.template.php
@@ -2,11 +2,12 @@
 <?php require 'partials/header.php'; ?>
 <?php require 'partials/banner.php'; ?>
 
-
-<form action="../includes/forgotpassword-include.php" method="post">
-	<label for="email">Email:</label>
-	<input type="email" id="email" name="email" required>
-	<input type="submit" value="Submit">
-</form>
+<main class="py-8">
+	<form class="mx-auto" action="../includes/forgotpassword-include.php" method="post">
+		<label for="email">Email:</label>
+		<input type="email" id="email" name="email" required>
+		<input type="submit" value="Submit">
+	</form>
+</main>
 
 <?php require 'partials/footer.php'; ?>
