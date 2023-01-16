e569a22d70d3ed95d51ed0f4855940fd6b319752
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:28:40 +0200

    Update 404.template.php

--- templates/404.template.php
+++ templates/404.template.php
@@ -1,3 +1,9 @@
-<h1>Lost in the Sea of Books? Let Us Help You Find Your Way</h1>
+<?php require 'partials/head.php'; ?>
 
-<? require 'partials/footer.php'; ?>
+<main class="flex flex-col items-center justify-center h-screen gap-4 p-8 font-bold tracking-tight">
+	<h1 class="text-2xl">Lost in the Sea of Books? Let Us Help You Find Your Way</h1>
+	<img src="/public/images/404.svg" class="max-w-xl" alt="404 error">
+	<a href="/" class="px-4 py-2 font-medium text-white bg-green-600 rounded">Go back home</a>
+</main>
+
+<?php require 'partials/footer.php'; ?>
