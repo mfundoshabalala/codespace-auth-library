a8b38ddd4408950fc4bf70b4eb551b4892feaa3f
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:28:22 +0200

    Create routes.php

---
+++ router/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'/' => 'controllers\index.php',
+	// '/about' => 'controllers\about.php',
+	'/books' => 'controllers\books.php',
+	'/book' => 'controllers\book\index.php',
+	'/book/create' => 'controllers\book\create.php',
+	// '/book/edit' => 'controllers\book\edit.php',
+	// '/book/delete' => 'controllers\book\delete.php',
+	'/register' => 'controllers\register.php',
+	'/login' => 'controllers\login.php',
+	// '/forgot-password' => 'controllers\forgot-password.php',
+	// '/reset-password' => 'controllers\reset-password.php',
+	// '/profile' => 'controllers\profile.php',
+];
