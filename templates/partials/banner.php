ed41ba0821259d392197681e1fbfaa6d9de81e09
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2023-01-17 00:28:13 +0200

    Update banner.php

--- templates/partials/banner.php
+++ templates/partials/banner.php
@@ -1,7 +1,8 @@
-<header class="bg-white shadow">
-	<div class="mx-auto py-6 px-4 sm:px-6 lg:px-8">
-		<h1 class="text-3xl font-bold tracking-tight text-gray-900">
-			<?php echo $heading; ?>
+<header class="px-1 rounded">
+	<div class="py-6 mx-auto border-b-2 border-gray-800 sm:px-6 lg:px-8">
+		<h1 class="text-3xl font-bold tracking-tight">
+			<span
+				class="max-w-fit bg-gradient-to-r <?= $gradient ?> bg-clip-text text-transparent"><?php echo $heading; ?></span>
 		</h1>
 	</div>
 </header>
